9829b27d9a4927bc1824d8739030776804b42711
LAIDOW-A <zaiadahmed535@gmail.com>
2026-02-03 14:42:08 +0200

    Refactor database connection to use DATABASE_URL

--- config/database.php
+++ config/database.php
@@ -1,14 +1,23 @@
 <?php
-$host = "localhost";        
-$dbname = "phpblog";        
-$username = "root";         
-$password = "";             
+$database_url = getenv('DATABASE_URL');
+
+if ($database_url) {
+    $url = parse_url($database_url);
+    $host = $url["host"];
+    $port = $url["port"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $dbname = ltrim($url["path"], '/');
+} else {
+    $host = "localhost";
+    $dbname = "phpblog";
+    $username = "root";
+    $password = "";
+}
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
