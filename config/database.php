bd77e69cedd005eb926dda626f6b60598eed673c
LAIDOW-A <zaiadahmed535@gmail.com>
2026-02-03 15:58:28 +0200

    Replace MySQL connection with MongoDB connection

--- config/database.php
+++ config/database.php
@@ -1,24 +1,11 @@
 <?php
-$database_url = getenv('DATABASE_URL');
-
-if ($database_url) {
-    $url = parse_url($database_url);
-    $host = $url["host"];
-    $port = $url["port"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $dbname = ltrim($url["path"], '/');
-} else {
-    $host = "localhost";
-    $dbname = "phpblog";
-    $username = "root";
-    $password = "";
-}
+$mongoUri = getenv('DATABASE_URL');
 
 try {
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+    $manager = new MongoDB\Driver\Manager($mongoUri);
+    $command = new MongoDB\Driver\Command(['ping' => 1]);
+    $manager->executeCommand('db', $command);
+} catch (Exception $e) {
+    die("Error: " . $e->getMessage());
 }
 ?>
